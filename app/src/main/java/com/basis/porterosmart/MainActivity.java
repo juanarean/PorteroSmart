48cde5685ff06ad4c338817cb37556628861e65b
Juan Arean <juan.arean@gmail.com>
2019-09-10 20:22:15 -0300

    Agrego pavadas

--- app/src/main/java/com/basis/porterosmart/MainActivity.java
+++ app/src/main/java/com/basis/porterosmart/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
+
+    TextView Revision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Revision = findViewById(R.id.tvRev);
+        Revision.setText("1.0.0");
     }
 
     @Override
